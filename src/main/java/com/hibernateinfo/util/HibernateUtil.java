14a8c61a179eb3b3179e6d9fd22e0d5ba046ed3d
Pasha <Pasha@Pasha>
2019-08-09 11:07:37 -0400

    Comment Added

--- src/main/java/com/hibernateinfo/util/HibernateUtil.java
+++ src/main/java/com/hibernateinfo/util/HibernateUtil.java
@@ -47,6 +47,9 @@ public class HibernateUtil
 		
 	}
 	//create static Utility Factory method to return sessionFactory
+	//A static method belongs to the class rather than the object of a class. 
+	//A static method can be invoked without the need for creating an instance of a class. 
+	//A static method can access static data member and can change the value of it.
 	public static SessionFactory getSessionFactory()
 	{
 		return sessionFactory;		
